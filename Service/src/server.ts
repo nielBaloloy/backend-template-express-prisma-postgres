3c63b6612b26c22560539a6ed8e01ad6f80a3c8b
Cris Niel Baloloy <cnbaloloy@pcdsi.ph>
2025-07-30 16:52:24 +0800

    add git ignore

--- Service/src/server.ts
+++ Service/src/server.ts
@@ -5,9 +5,5 @@ const app = express();
 app.use(express.json());
 app.use('/api', userRoutes); 
 
-app.get('/', (_req, res) => {
-  res.send('Hello from Express');
-});
-app.use(errorHandler);
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
